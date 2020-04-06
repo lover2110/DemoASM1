3ef7896f9175d75c6bb7f264ad24b09f553f2c48
lover2110 <62633334+lover2110@users.noreply.github.com>
2020-04-06 12:46:48 +0700

    111

--- login1/checkLogin.php
+++ login1/checkLogin.php
@@ -1,15 +1,15 @@
 <?php
 include 'connectdtb.php';
-    $username =$_POST['name'];
+   $username =$_POST['name'];
 //if(isset($_POST['password'])){
     $password=$_POST['pass'];
 //}
-$sql = "select * from tblacount where _name = '$username' and _pass= '$password'";
-$result = pg_result($dbconn, $sql);
-if(pg_fetch_row($result))
+$sql = "select * from tblaccount where _name = '".$username."' and _pass= '".$password."'";
+$result = pg_query($dbconn, $sql);
+if($result)
 {
   echo "Welcome to admin services !!!";
-  header('Location:home/home.php');
+  header('Location:../home.php');
 }
  else {
     echo "Oops...Have a trouble to Login. Please check again  :(("; 

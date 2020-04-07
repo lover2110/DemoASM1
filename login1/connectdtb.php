4bea06194727e8be1f705e01191da8ff227b5505
lover2110 <62633334+lover2110@users.noreply.github.com>
2020-04-07 11:46:45 +0700

    1146

--- login1/connectdtb.php
+++ login1/connectdtb.php
@@ -1,5 +1,5 @@
 <?php
-$conn_string = "host= ec2-52-71-231-180.compute-1.amazonaws.com port=5432 dbname=d1m2fqp06oji0j user=d1uofndelnmrd6 password=6dbeaa883d3b935ce696b759c80755e80ca2d534d9b9790bbdb5c565a5abbe7f";
+$conn_string = "host= ec2-52-71-231-180.compute-1.amazonaws.com port=5432 dbname=d1uofndelnmrd6 user=fzohixhwumzhrr password=6dbeaa883d3b935ce696b759c80755e80ca2d534d9b9790bbdb5c565a5abbe7f";
 $dbconn= pg_connect($conn_string);
 if($dbconn)
 {
